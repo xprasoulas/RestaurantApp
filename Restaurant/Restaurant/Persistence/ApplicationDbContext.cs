7f14eacab9bfd949e1aef0f7d0dfb1771c06668a
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-29 20:33:43 +0200

    Create ApplicationDbContext

---
+++ Restaurant/Restaurant/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Restaurant.Models;
+using System.Data.Entity;
+
+namespace Restaurant.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        // for the connection call with the AngularProject
+        public ApplicationDbContext() : base ("ApplicationDbContext")
+        {
+            base.Configuration.ProxyCreationEnabled = false; 
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
