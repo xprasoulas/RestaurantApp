40ae176f074d430a2b512febc70a705221ff0611
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-29 20:41:55 +0200

    Enable-Migrations

---
+++ Restaurant/Restaurant/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Restaurant.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Restaurant.Persistence.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Restaurant.Persistence.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
