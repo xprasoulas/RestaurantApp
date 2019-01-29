4bd3ffb2557abcb0abf2f9172907946960b8ff3c
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-29 21:27:30 +0200

    Add Controllers using WebAPI 2 with  action using EF. Remove from Customer&Item Controller unnecessary actions -Keep Orders ALL action

---
+++ Restaurant/Restaurant/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using Restaurant.Models;
+using Restaurant.Persistence;
+using System.Linq;
+using System.Web.Http;
+
+namespace Restaurant.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Customers
+        public IQueryable<Customer> GetCustomers()
+        {
+            return db.Customers;
+        }       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }        
+    }
+}
---
+++ Restaurant/Restaurant/Controllers/ItemsController.cs
@@ -0,0 +1,27 @@
+﻿using Restaurant.Models;
+using Restaurant.Persistence;
+using System.Linq;
+using System.Web.Http;
+
+namespace Restaurant.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Items
+        public IQueryable<Item> GetItems()
+        {
+            return db.Items;
+        }
+                
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }        
+    }
+}
