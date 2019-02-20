e2ad848a0f0f2830b1961e7b8b63f0362a549ad9
Charis Prasoulas <xprasoulas@gmail.com>
2019-02-20 22:43:56 +0200

    Add orderItems

--- RestaurantAngular/src/app/shared/order.service.ts
+++ RestaurantAngular/src/app/shared/order.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Order } from './order.model';
+import { OrderItem } from './order-item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-  formData: Order
+  formData: Order;
+  orderItems: OrderItem[] //to specify that we use an Array
 
   constructor() { }
 }
